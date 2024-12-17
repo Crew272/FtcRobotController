a05342e3b4681aad449fd869119687c64e8f6938
Crew272 <go.crew272@gmail.com>
2024-12-17 04:04:13 -0500

    Updated RobotHardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -20,10 +20,10 @@ public class RobotHardware {
 
     // Arm and Slide Motors
     public DcMotor armMotor;
-    public DcMotor slideMotor;
+    // public DcMotor slideMotor;
 
     // Intake Motor
-    public DcMotor intakeMotor;
+    // public DcMotor intakeMotor;
 
     // Servos
     public Servo grabberServo;
@@ -60,9 +60,9 @@ public class RobotHardware {
         setDrivetrainRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Odometry Wheels
-        centerOdometry = hardwareMap.dcMotor.get("CenterOdometry");
-        rightOdometry = hardwareMap.dcMotor.get("RightOdometry");
-        leftOdometry = hardwareMap.dcMotor.get("LeftOdometry");
+        centerOdometry = hardwareMap.dcMotor.get("centerOdometry");
+        rightOdometry = hardwareMap.dcMotor.get("rightOdometry");
+        leftOdometry = hardwareMap.dcMotor.get("leftOdometry");
 
         // Configure odometry wheels for tracking
         centerOdometry.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -74,25 +74,25 @@ public class RobotHardware {
         leftOdometry.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         // Arm and Slide Motors
-        armMotor = hardwareMap.dcMotor.get("arm_motor");
-        slideMotor = hardwareMap.dcMotor.get("slide_motor");
+        armMotor = hardwareMap.dcMotor.get("armMotor");
+        // slideMotor = hardwareMap.dcMotor.get("slide_motor");
 
         // Configure arm and slide motors
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+       //  slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Intake Motor
-        intakeMotor = hardwareMap.dcMotor.get("intake_motor");
+        //intakeMotor = hardwareMap.dcMotor.get("intake_motor");
 
         // Servos
-        grabberServo = hardwareMap.servo.get("grabber_servo");
-        intakePositionServo = hardwareMap.servo.get("intake_position_servo");
+        grabberServo = hardwareMap.servo.get("grabberServo");
+        intakePositionServo = hardwareMap.servo.get("intakePositionServo");
 
         // Touch Sensors (Limit Switches)
-        armZeroSwitch = hardwareMap.touchSensor.get("arm_zero_switch");
-        slideZeroSwitch = hardwareMap.touchSensor.get("slide_zero_switch");
+        armZeroSwitch = hardwareMap.touchSensor.get("armZeroSwitch");
+        slideZeroSwitch = hardwareMap.touchSensor.get("slideZeroSwitch");
     }
 
     // Method to set drivetrain motor run modes
@@ -104,9 +104,9 @@ public class RobotHardware {
     }
 
     // Method to control intake
-    public void setIntakePower(double power) {
-        intakeMotor.setPower(power);
-    }
+    // public void setIntakePower(double power) {
+    //    intakeMotor.setPower(power);
+    //}
 
     // Method to control grabber servo
     public void setGrabberPosition(double position) {
